2882b01059f15f13a64365677098261d2079953a
MumenAlmadaineh <mumen_almadaineh@outlook.com>
2021-09-28 20:54:12 +0300

    Fix auth0

--- src/index.js
+++ src/index.js
@@ -15,9 +15,13 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+
+
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.js";
 import RTLLayout from "layouts/RTL/RTL.js";
@@ -31,20 +35,83 @@ import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
 import HomePage from "./components/homepage.js"
 
+
+import App from "App";
+
 ReactDOM.render(
-  <ThemeContextWrapper>
-    <BackgroundColorWrapper>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <HomePage/>
-          </Route>
-          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-          <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
-          {/* <Redirect from="/" to="/admin/dashboard" />  */}
-        </Switch>
-      </BrowserRouter>
-    </BackgroundColorWrapper>
-  </ThemeContextWrapper>,
+  <Auth0Provider
+    domain={`dev-35i0ibe7.us.auth0.com`}
+    clientId={`uqSOedMRIEudQUlX7dc3HoieE9F76fUn`}
+    redirectUri={`http://localhost:3000/`}
+  >
+    <App />
+    <ThemeContextWrapper>
+      <BackgroundColorWrapper>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+            <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
+            {/* <Redirect from="/" to="/admin/dashboard" />  */}
+          </Switch>
+        </BrowserRouter>
+      </BackgroundColorWrapper>
+    </ThemeContextWrapper>,
+  </Auth0Provider>,
   document.getElementById("root")
 );
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import { Auth0Provider } from "@auth0/auth0-react";
+// ReactDOM.render(
+//   <Auth0Provider
+//     domain={process.env.REACT_APP_AUTH0_DOMAIN}
+//     clientId={process.env.REACT_APP_AUTH0_CLIENTID}
+//     redirectUri={`${process.env.REACT_APP_AUTH0_PORT}`}
+//   >
+//     <App />
+//   </Auth0Provider>,
+//   document.getElementById("root")
+// );
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Route, Switch} from "react-router-dom";
+
+// import AdminLayout from "layouts/Admin/Admin.js";
+// import RTLLayout from "layouts/RTL/RTL.js";
+
+// import "assets/scss/black-dashboard-react.scss";
+// import "assets/demo/demo.css";
+// import "assets/css/nucleo-icons.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
+// import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+// import HomePage from "./components/homepage.js"
+
+// ReactDOM.render(
+//   <ThemeContextWrapper>
+//     <BackgroundColorWrapper>
+//       <BrowserRouter>
+//         <Switch>
+//           <Route exact path="/">
+//             <HomePage/>
+//           </Route>
+//           <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+//           <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
+//           {/* <Redirect from="/" to="/admin/dashboard" />  */}
+//         </Switch>
+//       </BrowserRouter>
+//     </BackgroundColorWrapper>
+//   </ThemeContextWrapper>,
+//   document.getElementById("root")
+// );
+
+
