fa9b38b31bc4af7edc59aa916b40784622d41fca
Razan Armouti <razanarmote@yahoo.com>
2021-09-28 21:10:11 +0300

    auth0 solved

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import LoginButton from "components/LoginButton";
+import LogoutButton from "components/LogoutButton";
+import { withAuth0 } from "@auth0/auth0-react";
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <LoginButton />
+                <LogoutButton />
+            </div>
+        )
+    }
+}
+export default withAuth0(App)
--- src/index.js
+++ src/index.js
@@ -15,39 +15,81 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { BrowserRouter, Route, Switch} from "react-router-dom";
+
+// import App from "App";
+
+// import AdminLayout from "layouts/Admin/Admin.js";
+// import RTLLayout from "layouts/RTL/RTL.js";
+
+// import "assets/scss/black-dashboard-react.scss";
+// import "assets/demo/demo.css";
+// import "assets/css/nucleo-icons.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
+// import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
+// import HomePage from "./components/homepage.js";
+// import { Auth0Provider } from "@auth0/auth0-react";
+
+
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch} from "react-router-dom";
-
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import AdminLayout from "layouts/Admin/Admin.js";
 import RTLLayout from "layouts/RTL/RTL.js";
-
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
 import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
-import HomePage from "./components/homepage.js";
-//import { Auth0Provider } from "@auth0/auth0-react";
+import HomePage from "./components/homepage.js"
+import App from "App";
 
 ReactDOM.render(
- 
+
+<Auth0Provider
+    domain={`dev-35i0ibe7.us.auth0.com`}
+    clientId={`uqSOedMRIEudQUlX7dc3HoieE9F76fUn`}
+    redirectUri={`http://localhost:3000/`}
+  >
+    <App />
     <ThemeContextWrapper>
-    <BackgroundColorWrapper>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <HomePage/>
-          </Route>
-          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-          <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
-          {/* <Redirect from="/" to="/admin/dashboard" />  */}
-        </Switch>
-      </BrowserRouter>
-    </BackgroundColorWrapper>
-  </ThemeContextWrapper>,
+      <BackgroundColorWrapper>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+            <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
+            {/* <Redirect from="/" to="/admin/dashboard" />  */}
+          </Switch>
+        </BrowserRouter>
+      </BackgroundColorWrapper>
+    </ThemeContextWrapper>,
+  </Auth0Provider>,
+ 
+
+
+  //   <ThemeContextWrapper>
+  //   <BackgroundColorWrapper>
+  //     <BrowserRouter>
+  //       <Switch>
+  //         <Route exact path="/">
+  //           <HomePage/>
+  //         </Route>
+  //         <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+  //         <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
+  //         {/* <Redirect from="/" to="/admin/dashboard" />  */}
+  //       </Switch>
+  //     </BrowserRouter>
+  //   </BackgroundColorWrapper>
+  // </ThemeContextWrapper>,
   
  
   document.getElementById("root")
